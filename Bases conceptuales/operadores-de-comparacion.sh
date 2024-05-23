e7aa6a3c23d8c97775bcd26378366fc62a20435a
liocutro.cs <lionel.cutro@gmail.com>
2024-05-23 19:39:44 -0300

    Create operadores-de-comparacion.sh
    operators

---
+++ Bases conceptuales/operadores-de-comparacion.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+# Existen 3 bloques de operadores de comparacion,
+# cada bloque pertene a un ambito diferente.
+
+
+# Comparacion de Cadenas;
+
+#  =   -> igual a
+#  !=  -> no igual a 
+#  >   -> mayor que (lexicograficamente)
+#  <   -> menor que (lexicograficamente)
+
+#------------------------------------------------
+
+# Comparacion de Archivos;
+
+#  -e   -> existe
+#  -f   -> es un archivo regular
+#  -d   -> es un directorio
+#  -r   -> permite lectura
+#  -w   -> permite escritura
+#  -x   -> permite ejecutarse
+
+#------------------------------------------------
+
+# Comparacion de Numeros;
+
+#  -eq  -> igual a
+#  -ne  -> no igual a
+#  -lt  -> menor que 
+#  -le  -> menor o igual que
+#  -gt  -> mayor que
+#  -ge  -> mayor o igual que
